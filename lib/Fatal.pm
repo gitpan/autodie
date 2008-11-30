1d9bed1482a0f3bfe9c883fafc9ca2b93157c547
Paul Jamieson Fenwick <pjf@perltraining.com.au>
2008-11-30 06:37:02 +0000

    Import of PJF/autodie-1.995 from CPAN.
    gitpan-cpan-distribution: autodie
    gitpan-cpan-version:      1.995
    gitpan-cpan-path:         PJF/autodie-1.995.tar.gz
    gitpan-cpan-author:       PJF
    gitpan-cpan-maturity:     released

--- lib/Fatal.pm
+++ lib/Fatal.pm
@@ -31,7 +31,7 @@ use constant ERROR_FATAL_CONFLICT => q{"use Fatal '%s'" is not allowed while "no
 use constant MIN_IPC_SYS_SIMPLE_VER => 0.12;
 
 # All the Fatal/autodie modules share the same version number.
-our $VERSION = '1.994';
+our $VERSION = '1.995';
 
 our $Debug ||= 0;
 
@@ -376,6 +376,9 @@ sub unimport {
 }
 
 # TODO - This is rather terribly inefficient right now.
+
+# NB: Perl::Critic's dump-autodie-tag-contents depends upon this
+# continuing to work.
 
 {
     my %tag_cache;
