0170fc25abbe516c09c0d20c6b093c45896b946a
Paul Jamieson Fenwick <pjf@perltraining.com.au>
2013-05-13 00:03:25 +0000

    Import of PJF/autodie-2.19 from CPAN.
    gitpan-cpan-distribution: autodie
    gitpan-cpan-version:      2.19
    gitpan-cpan-path:         PJF/autodie-2.19.tar.gz
    gitpan-cpan-author:       PJF
    gitpan-cpan-maturity:     released

--- t/autodie_test_module.pm
+++ t/autodie_test_module.pm
@@ -2,6 +2,10 @@ package main;
 use strict;
 use warnings;
 
+use constant NOFILE1 => 'this_file_had_better_not_exist';
+use constant NOFILE2 => NOFILE1 . '2';
+use constant NOFILE3 => NOFILE1 . '3';
+
 # Calls open, while still in the main package.  This shouldn't
 # be autodying.
 sub leak_test {
@@ -11,6 +15,15 @@ sub leak_test {
 # This rename shouldn't be autodying, either.
 sub leak_test_rename {
     return rename($_[0], $_[1]);
+}
+
+# These are used by core-trampoline-slurp.t
+sub slurp_leak_unlink {
+    unlink(NOFILE1, NOFILE2, NOFILE3);
+}
+
+sub slurp_leak_open {
+    open(1,2,3,4,5);
 }
 
 package autodie_test_module;
